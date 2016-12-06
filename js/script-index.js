6e95321c0df97bbe5e423f637f1029a7aa96a892
NayeMR <naye.mirandys@gmail.com>
2016-12-06 08:50:24 -0900

    ETAPA-4

--- js/script-index.js
+++ js/script-index.js
@@ -3,49 +3,70 @@ $(document).ready( function(){
 	renderHighlightedRecipes(recipesArray);
 	$('.js-back').hide();
 	printNews();
+	
 
 });
 /*Funcion para agregar parrafo*/
  function  printNews(event){
  	$('#parrafoCallout').text('"NUEVAS RECETAS"');
  }
-
-
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
-		 for(var i = 0; i < recipesArray.length; i++)
-		{
-			if(recipesArray[i].highlighted== true){
-				//console.log('elemento que lo tiene es: '+ i);
-				renderRecipe(i);
-			}
+	for(var i = 0; i < recipesArray.length; i++)
+	{
+		if(recipesArray[i].highlighted == true){
+			//console.log('elemento que lo tiene es: '+ i);
+			renderRecipe(recipesArray[i]);
 		}
-		
+	}
 }
-
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
 * marcado el atributo "highlighted" como TRUE
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
+//URL PARA LAS RECETAS
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+	//console.log('Voy a pintar la receta: ', recipe);
+	
+	//elemento adentro del div
+	var a = $('<a class="item-recipe" href="#"></a>');
+	var spanAttribution = $('<span class="attribution"></span>');
+	var spanTitleRecipe = $('<span class="title-recipe"></span>'); 
+	spanTitleRecipe.text(recipe.title);
+	var spanMetadataRecipe = $('<span class="metadata-recipe"></span>');
+	var spanAuthorRecipe = $('<span class="author-recipe"></span>');
+	spanAuthorRecipe.text(recipe.source.name);
+	var spanBookmarksRecipe = $('<span class="bookmarks-recipe"></span>');
+	var sapanIconBookmark = $('<span class="icon-bookmark"></span>');
+
+	//imagen 
+	var img = $('<img>');
+	img.attr('src', "img/recipes/640x480/"+ recipe.name +".jpg");
+
+
+	a.append(spanAttribution);
+
+	spanAttribution.append(spanMetadataRecipe);
+	spanAttribution.append(spanTitleRecipe);
+	spanMetadataRecipe.append(spanAuthorRecipe);
+	spanMetadataRecipe.append(spanBookmarksRecipe);
+	spanBookmarksRecipe.append(sapanIconBookmark);
+	a.append(img);
+	//agrega el a al div
+	$('.list-recipes').append(a);
 }
-
-
-
 /*
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
 }
-
 /*
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
