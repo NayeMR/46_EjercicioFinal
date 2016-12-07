329e34d02afb320e6f8ed5b11803afb65c2ba999
NayeMR <naye.mirandys@gmail.com>
2016-12-07 08:54:44 -0900

    ETAPA-8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,19 +1,20 @@
 $(document).ready( function(){
-	removeClase();
 	addClase();
+	removeClase();
 	$('.icon-menu').hide();
 	//console.log("HTML LISTO");
 	
 });
+/*Funcion para agregar la clase make*/
+function addClase(){
+	$('.js-show-make').click(function () {
+	   $('.page, .recipe').addClass('make');
+	});
+}
+/*funcion para remover la clase make*/
 function removeClase() {
   $('.js-show-recipe').click(function () {
 	   $('.page, .recipe').removeClass('make');
 	   
-	});
-}
-function addClase(){
-	$('.js-show-make').click(function () {
-	    
-	   $('.page, .recipe').addClass('make');
 	});
 }
