5b143b14302bae81ba245c5e82081996dae38301
NayeMR <naye.mirandys@gmail.com>
2016-12-07 09:14:05 -0900

    ETAPA-9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,16 +5,19 @@ $(document).ready( function(){
 	//console.log("HTML LISTO");
 	
 });
-/*Funcion para agregar la clase make*/
+/*Funcion para agregar la clase make y modifica la clase active*/
 function addClase(){
 	$('.js-show-make').click(function () {
 	   $('.page, .recipe').addClass('make');
+	   $('.js-show-make').addClass('active');
+	   $('.js-show-recipe').removeClass('active'); 
 	});
 }
-/*funcion para remover la clase make*/
+/*funcion para remover la clase make y modifica la clase active*/
 function removeClase() {
   $('.js-show-recipe').click(function () {
 	   $('.page, .recipe').removeClass('make');
-	   
+	   $('.js-show-recipe').addClass('active'); 
+	   $('.js-show-make').removeClass('active'); 
 	});
 }
